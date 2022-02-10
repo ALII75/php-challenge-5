1549eac5af0ecca5879953efdc4fd9b3c9337ae1
Selmani <selmaniali@yahoo.com>
2022-02-10 14:47:22 +0100

    stevenson et opponent

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$weapons = ['fists', 'whip', 'gun'];
+$opponentWeapon = $weapons[rand(0,2)];
+
+if ($opponentWeapon === $weapons[0]) {
+    $stevensonWeapon = $weapons[2];
+} elseif ($opponentWeapon === $weapons[1]) {
+    $stevensonWeapon = $weapons[0];
+} else {
+    $stevensonWeapon = $weapons[1];
+}
+
+echo " $opponentWeapon vs  $stevensonWeapon";
